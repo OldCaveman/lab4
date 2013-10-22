f4979b1f7bcbf024ac513f9db3c3fba9771af426
Alex Caven <apcaven@gmail.com>
2013-10-21 23:29:40 -0400

    extract values code

--- download_and_extract.py
+++ download_and_extract.py
@@ -1,14 +1,20 @@
 import urllib2
 import csv
 
-
 def download(datetime):
-    # parse out the date into the url string
-    url = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KNYTULLY1&month=10&day=32&year=2008&format=1"
-    response = urllib2.urlopen(url).read()
-    return response
+	# parse out the date into the url string
+	url = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KNYTULLY1&month=10&day=32&year=2008&format=1"
+	response = urllib2.urlopen(url).read()
+	accurate = response.replace('\n<br>', "")
+	return accurate
 
 def extract_date_time_and_temp(downloaded):
-    results = []
-    # use csvreader
-    return results
+	results = []
+	for measurement in downloaded.split("\n"):
+		interim = measurement.split(',')
+		results.append(interim[0:1])
+	# use csvreader
+	return results
+
+for measurement in downloaded.split("\n"):
+	result = measurement.split(',')
