2970de163f0925ad43728c5758837c7b4bd8c34a
Alex Caven <apcaven@gmail.com>
2013-10-22 00:16:01 -0400

    lab4 updates

--- lab4.py
+++ lab4.py
@@ -1,10 +1,11 @@
-
 import time
 import datetime
 import urllib
 import webbrowser
 import os
 import csv
+from download_and_extract import*
+
 
 # data source
 url = 'http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KNYTULLY1&month=%i&day=%i&year=%i&format=1'
@@ -14,7 +15,7 @@ url = 'http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KNYTULLY
 #create a file for the data to be saved
 ##os.chdir(r'H:\Desktop')
 ##inp = file('Tullydata.csv', 'r')
-outp = file('Caven_Tullydata.csv', 'w')
+##outp = file('Caven_Tullydata.csv', 'w')
 #begin saved data file with header of dates
 ##header = outp.readline().strip().split(',')
 
@@ -61,46 +62,42 @@ day1 = datetime.date(2006, 1,1)
 ##    print time.strptime(thisTime, "%Y %j")
 
 #Provide true statements for reference
-a = 0
-b = 1
 #Loop through three years of days (+1 for leap year 2008)
 for i in range(1096):
     thisTime = day1 + datetime.timedelta(i)
-    data = url % (thisTime.month, thisTime.day, thisTime.year)
-    time = urllib.urlopen(data)
+    downloaded = download(thisTime)
+    extracted = extract(downloaded)
+    with open(Caven_Tullydata.csv, 'wa') as csvfile:
+        mywriter = csv.writer(csvfile)
+        mywriter.writerows(extracted)
+        print extracted
+##    data = url % (thisTime.month, thisTime.day, thisTime.year)
+##    time = urllib.urlopen(data)
 #loop through times
-    for j in time:
-        if a == 0:
-            print ''
-        elif a == 2:
-            if b == 1:
+##    for j in time:
+##        if a == 0:
+##            print ''
+##        elif a == 2:
+##            if b == 1:
 #write header "datetime and temp"
-                header = j.split(',')
-                outp.write(header[0] + "," + header[1] + "\n")
-                b = 2
-        elif j == '<br>\n':
-            print ''
+##                header = j.split(',')
+##                outp.write(header[0] + "," + header[1] + "\n")
+##                b = 2
+##        elif j == '<br>\n':
+##            print ''
 #Split temp and datetime strings
-        else:
-            m = j.split(',')
-            n = m[0]
+##        else:
+##            m = j.split(',')
+##            n = m[0]
 #Handle exceptions (not sure why these are occuring really. url contains bad or non-existent data?)
-            try:
-                o = m[1]
-            except:
-                IndexError
+##            try:
+##                o = m[1]
+##            except:
+##                IndexError
 #write temp and datetime to output file
-            outp.write(n + ',' + o + '\n')
+##            outp.write(n + ',' + o + '\n')
 ##            print n
 ##            print o
-        a = a + 1
-outp.close()
-
-    
-
-
-
-
-
-
+##        a = a + 1
+##outp.close()
 
